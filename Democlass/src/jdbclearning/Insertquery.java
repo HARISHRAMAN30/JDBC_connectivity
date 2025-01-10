da744f5103160dedeff9e44057f5c08723ca833f
Harishraman <ramanharish30@gmail.com>
2025-01-11 01:29:06 +0530

    JAVA JDBC UPDATE AND FETCH QUERIES

--- Democlass/src/jdbclearning/Insertquery.java
+++ Democlass/src/jdbclearning/Insertquery.java
@@ -8,19 +8,33 @@ public class Insertquery {
 		String url = "jdbc:mysql://localhost:3306/jdbc_connectivity";
 		String uname = "root";
 		String pass = "root";
-//		int id = 2;
-//		String name = "def";
-//		String major = "poi";
-//		int age = 45;
+		
+//		int id = 3;
+//		String name = "ghi";
+//		String major = "lkj";
+//		int age = 32;
+
 		String query = "Insert into staffs values (1,'abc','qwe',22)"; //two ways
+		
 //		String query = "Insert into staffs values (" + id + ",'" + name + "','" + major + "'," + age + ")";		
+// 	    String query = "Insert into staffs values (?,?,?,?)"; //two ways
+		
 		Class.forName("com.mysql.cj.jdbc.Driver");
 		Connection con = DriverManager.getConnection(url, uname, pass);
 		Statement st = con.createStatement();
+		
+//		PreparedStatement st = con.prepareStatement(query);
+//		st.setInt(1, id);
+//		st.setString(2, name);
+//		st.setString(3, major);
+//		st.setInt(4, age);
+		
 		int count = st.executeUpdate(query);
 		
+//		int count = st.executeUpdate();
+		
 		System.out.println(count + " rows affected");
-			
+	
 		st.close();
 		con.close();
 		
